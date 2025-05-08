474d56d21e4289d5e2080fe2f2113f5af16b2d72
pavli <pavli@NOTEBOOK-PAVEL>
2025-05-09 00:41:42 +0200

    finished inference implementation

--- AlignAtt/alignatt_visualization.py
+++ AlignAtt/alignatt_visualization.py
@@ -4,7 +4,28 @@ import torch
 import evaluate
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, GenerationConfig
+import argparse
+import jsonlines
+from itertools import islice
+def parse_args():
+    parser = argparse.ArgumentParser()
+    id = 0
+    keys = ["czech", "english"] if False else ["pref_source", "pref_target"]
+    parser.add_argument("--skip_l", type=int, default=0, help="Number of last positions in attention_frame_size to ignore")
+    parser.add_argument("--layers", type=int, nargs='+', default=[4], help="List of layer indices")
+    parser.add_argument("--top_attentions", type=int, default=10, help="Top attentions to use, set to zero to disable alignatt.")
+    parser.add_argument("--attention_frame_size", type=int, default=10, help="The excluded frame of last positions size")
+    parser.add_argument("--count_in", type=int, default=5, help="How many values in the top_attentions must be in attention_frame_size from end for the position to be bad.")
+    parser.add_argument("--heads", type=int, nargs='+', default=list(range(6)), help="List of attention heads")
+    parser.add_argument("--device", type=str, default="cuda", help="Device to use")
+    parser.add_argument("--words_per_prefix", type=int, default=2, help="Words per prefix shown")
+    parser.add_argument("--forced_bos_token_text", type=str, default=None, help="Forced BOS token text")
+    parser.add_argument("--model_id", type=int, default=id, help="Model ID")
+    parser.add_argument("--num_beams", type=int, default=10, help="Setting the num_beams to a multiple of three turns on diverse beam search with num_beams//3 groups.")
+    parser.add_argument("--src_key", type=str, default=keys[0], help="Source key")
+    parser.add_argument("--tgt_key", type=str, default=keys[1], help="Target key")
 
+    return parser.parse_args()
 
 class States:
     def __init__(self):
@@ -54,15 +75,19 @@ def local_agreement(self, states, new_hypothesis, segment_finished, tokenizer):
 
 def make_alignments(datap, args):
     alignments = []
-    src = datap["czech"].split(" ")
-    tar = datap["english"].split(" ")
+    src = datap[args.src_key]
+    tgt = datap[args.tgt_key]
+    src_words = src.split(" ")
+    tar_words = tgt.split(" ")
 
     # alligning the full English sentences with all Czech substrings combinations
-    for x in range(0, len(src), args.words_per_prefix):
-        alignments.append((" ".join(src[0:x + args.words_per_prefix]), datap["english"]))
+    for x in range(0, len(src_words), args.words_per_prefix):
+        alignments.append((" ".join(src_words[0:x + args.words_per_prefix]), tgt))
         # alignments.append((" ".join(src[0:x+5]), [" ".join(tar[0:y+5] for y in range(len(src)))]))
     return alignments
 
+def make_pair(datap, args):
+    return datap[args.src_key], datap[args.tgt_key]
 
 def visualize_attention(input_ids, output_ids, attentions, tokenizer, args):
     def sort_top(l, t):
@@ -95,7 +120,7 @@ def alignatt(attentions, args):
         if np.sum(np.less_equal(attentions[0].shape[-1] - args.attention_frame_size, top_pos)) > args.count_in:
             print(i, len(attentions), attentions[0].shape[-1] - top_pos)
             return i
-    print(len(attentions), attentions[0].shape[-1] - top_pos)
+    print(len(attentions), "full", attentions[0].shape[-1] - top_pos)
     return len(attentions)
 
 
@@ -113,7 +138,7 @@ def translate(model, tokenizer, input_text, stable_theory, args, verbose=False):
       Tuple (one element for each generated token) of tuples (one element for each layer of the decoder) of torch.FloatTensor
       of shape (batch_size, num_heads, generated_length, sequence_length).
     """
-    config = GenerationConfig(num_beams=args.num_beams, num_beam_groups=args.num_beams//3 if args.num_beams % 3 == 0 else 0, diversity_penalty=0.1, no_repeat_ngram_size=2,
+    config = GenerationConfig(num_beams=args.num_beams, num_beam_groups=args.num_beams//3 if args.num_beams % 3 == 0 else 1, diversity_penalty=0.1 if args.num_beams % 3 == 0 and args.num_beams > 3 else 0, no_repeat_ngram_size=2,
                               length_penalty=0.98)
     outputs = model.generate(input_ids=input_ids.to(args.device), decoder_input_ids=decoder_input_ids.to(
         args.device) if decoder_input_ids is not None else None,
@@ -137,10 +162,17 @@ def translate(model, tokenizer, input_text, stable_theory, args, verbose=False):
 
 
 def analyze_dataset(args):
-    data = json.load(open("iwslt2024_cs_devset.json"))
-    # flattens all the allignments into 1 dimension list
-    prefixes = [make_alignments(data[i], args) for i in range(len(data))]
-
+    with jsonlines.open("prefixes_dataset.jsonl") as reader:
+        data = list(islice(reader, 200))
+    words = [make_pair(data[i], args) for i in range(len(data))]
+    prefixes = [[words[0]]]
+    for x in words[1:]:
+        print(x[0], prefixes[-1][-1][0])
+        if x[0].startswith(prefixes[-1][-1][0]):
+            prefixes[-1].append(x)
+        else:
+            prefixes.append([x])
+    print([len(x) for x in prefixes])
     ''''
       the model names used
       first model will be compared to the other modesl
@@ -157,9 +189,12 @@ def analyze_dataset(args):
     bleu = evaluate.load("bleu")
     total_bleu = 0
     total_delay = 0
-    # The number of prefixes seen.
+    # The total number of prefixes seen.
     cs = 0
+    wait_for = 2
     for x in prefixes[:10]:
+        if len(x) < wait_for+3:
+            continue
         wordsen = x[-1][1].split(" ")
         # We prefix it with some text to not start the translation from nothing.
         helpt = False
@@ -174,44 +209,42 @@ def analyze_dataset(args):
         previous_theory = stable_theory
         # How much is the english text longer than the czech text.
         frac = len(x[-1][1]) / len(x[-1][0]) + 0.5
-        for t in range(int(start * frac), len(x)):
+        s = int(start * frac)
+        for t in range(s, len(x)):
             # print(frac * len(x[t][0]),  len(stable_theory), len(x[t][0]), len(x[-1][1]))
             new_delay = (frac * len(x[t][0])) - len(stable_theory)
             total_delay += new_delay
-            if t < 5:
+            if t < wait_for:
                 continue
             new_theory = translate(model, tokenizer, helper_text + x[t][0], stable_theory, args, False)
-            # If we begin repeating the same tokens, we dont take the output.
+            # If we begin repeating the same tokens, we don't take the output.
             newtokens = new_theory[len(stable_theory):]
             if np.unique(newtokens).shape[0] < len(newtokens) // 2:
                 print("repeating tokens")
-                stable_theory += tokenizer.tokenize(" ")
+                stable_theory += [tokenizer.tokenize(" ")]
                 continue
-            for i in range(len(stable_theory), min(len(new_theory), len(previous_theory))):
-                if new_theory[i:i+2] != previous_theory[i:i+2] or len(new_theory) > 400:
+            stop = min(len(new_theory), len(previous_theory))
+            for i in range(len(stable_theory), stop):
+                if any([new_theory[j] != previous_theory[j] for j in range(i, min(stop, i+1))]) or len(new_theory) > 500:
+                    print(new_theory[i], previous_theory[i])
                     break
-                stable_theory += new_theory[i]
+                stable_theory += [new_theory[i]]
             print("****", len(new_theory) - len(stable_theory))
             print(x[t][0])
             print("".join(stable_theory).replace("▁", " ")[lhten:])
             print("".join(new_theory).replace("▁", " ")[lhten:])
             print(x[-1][1])
             previous_theory = new_theory
-        new_bleu = bleu.compute(predictions=["".join(new_theory).replace("▁", " ")], references=[x[-1][1]])["bleu"]
+        new_bleu = bleu.compute(predictions=["".join(new_theory if False else stable_theory).replace("▁", " ")], references=[x[-1][1]])["bleu"] if len(stable_theory) > 0 else 0
         total_bleu += new_bleu
         cs += 1
         print(new_bleu, total_bleu / cs, new_delay, total_delay / cs)
-
 
 # default 0.28967596904893456 0.21614738454887503 71.79527559055117 59.460164212070396
 # -100 0.1857398572730801 0.24759903978731826 41.23529411764707 158.65644156457532
 
 def main():
-    id = 2
-    #Setting the num_beams to a multiple of three turn on diverse beam search with num_beams//3 beams.
-    argsdict = {"skip_l": 0, "layers": [4], "top_attentions": 0, "attention_frame_size": 10, "count_in": 5,
-                "heads": list(range(6)), "device": "cuda", "words_per_prefix": 2, "forced_bos_token_text": "eng_Latn" if id > 0 else None, "model_id": id, "num_beams": 2}
-    args = Namespace(**argsdict)
+    args = parse_args()
     analyze_dataset(args)
 
 
