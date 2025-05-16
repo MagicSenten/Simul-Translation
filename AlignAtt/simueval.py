892babb0597a4fcf5ad82f6181137261f44d9e0f
OriMoscovitz <ororior208@gmail.com>
2025-05-16 11:22:35 +0200

    Added parital implementation for AL Metric

--- AlignAtt/simueval.py
+++ AlignAtt/simueval.py
@@ -1,48 +1,110 @@
+from typing import List, Union
+# added comment for commit
+
+def calc_latency_ratio(partial, full, pred, gold):
+    return (len(gold) / len(full) * len(partial) - len(pred)) / len(gold)
+
+
+def compute(
+        delays: List[Union[float, int]],
+        source_length: Union[float, int],
+        target_length: Union[float, int],
+):
+    """
+    Function to compute latency on one sentence (instance).
+
+    Args:
+        delays (List[Union[float, int]]): Sequence of delays.
+        source_length (Union[float, int]): Length of source sequence.
+        target_length (Union[float, int]): Length of target sequence.
+
+    Returns:
+        float: the latency score on one sentence.
+    """
+
+    if delays[0] > source_length:
+        return delays[0]
+
+    AL = 0
+    gamma = target_length / source_length
+    tau = 0
+    for t_miuns_1, d in enumerate(delays):
+        if d <= source_length:
+            AL += d - t_miuns_1 / gamma
+            tau = t_miuns_1 + 1
+
+            if d == source_length:
+                break
+    AL /= tau
+    return AL
+
+
 class SimuEval:
     def __init__(self):
+        self.gold_text = ""
+        self.cur_pred_pos = 0
+
+        self.delays = []
+
         self.latency_scorer = []
         self.quality_scorer = []
 
-        self.chars__latency = 0
         self.words__latency = 0
-        self.tokens_latency = 0
+        # self.chars__latency = 0
+        # self.tokens_latency = 0
 
         self.cs = 0
 
-    def update(self, partial_input_text, full_input_text, predicted_text,
-               gold_text, bpe_tokenizer):
-        # shortened names for partial_input_text, full_input_text, predicted_text, gold_text, bpe_tokenizer
-        pit, fit, pt, gt, t = partial_input_text, full_input_text, predicted_text, gold_text, bpe_tokenizer
-        chars_delay = self.calc_latency_ratio(pit, fit, pt, gt)
+    def update(self, inputs, pred_outputs, gold_text, tokenizer):
+        print(f"inputs: {inputs}")
+        # init current delay list
+        cur_delay = []
 
-        words_pit, words_fit, words_pt, words_gt = pit.split(" "), fit.split(" "), pt.split(" "), gt.split(" ")
-        words_delay = self.calc_latency_ratio(words_pit, words_fit, words_pt, words_gt)
+        # the length of the inputs and output
+        prefix_len = len(inputs)
 
-        tokens_delay = self.calc_latency_ratio(words_pit, words_fit, words_pt, words_gt)
+        for i in range(prefix_len):
+            pred_len = len(pred_outputs[i].strip().split(" "))
+            for _ in range(pred_len):
+                inp_len = len(inputs[i].strip().split(" "))
+                cur_delay.append(inp_len)
 
-        new_delay_tokens = (frac_tokens * len(
-            bpe_tokenizer.tokenize(partial_input_text))) - len(
-            bpe_tokenizer.tokenize(predicted_text)) / len(tokens_en)
-        new_bleu = bleu.compute(input_text, gold_text)
+            self.cur_pred_pos = pred_len
+        self.delays.append(cur_delay)
 
-    # return the latency ratio for given chars, words or tokens
-    def calc_latency_ratio(self, partial, full, pred, gold):
-        return (len(gold) / len(full) * len(partial) - len(pred)) / len(gold)
-
-    # figure out only what g is
-    def average_lagging(self, prefixes, source, target, stable_len):
-        al_sum = 0
-        r = len(target) / len(source)
-
-        for i in range(1, stable_len):
-            al_sum += g - (i - 1) / r
-
-        return (1 / stable_len) * al_sum
+        # # marks the new iteration of a new gold_label
+        # if self.gold_text != gold_text:
+        #     '''
+        #         init for each pair:
+        #         - save the delay list for that pair in the delays list
+        #         - init cur_delay list
+        #         - init cur_pred_pos
+        #     '''
+        #     self.gold_text = gold_text
+        #     # only append non empty lists
+        #     if len(self.cur_delay) > 0:
+        #         self.delays.append(self.cur_delay)
+        #     self.cur_delay = []
+        #     self.cur_pred_pos = 0
+        #
+        # # calcs how many words were predicted, makes sure to clean trailing spaces before splitting
+        # pred_len = len(predicted_text.strip().split(" "))
+        # # calcs the position we had to read to in input to get the stabalized output
+        # part_len = len(partial_input_text.split(" "))
+        #
+        # for i in range(self.cur_pred_pos, pred_len):
+        #     self.cur_delay.append(part_len)
+        #
+        # # update the cur position to be read to
+        # self.cur_pred_pos = pred_len
+        #
+        # # returns the updated delays list
+        # return self.delays
+        return None
 
     def eval(self):
-        return ""
         return {
-            "delay_chars": self.delay_chars,
-            "delay_words": self.delay_words,
-            "delay_tokens": self.delay_tokens,
+            "delay_words": self.words__latency,
+            # "delay_chars": self.chars__latency,
+            # "delay_tokens": self.tokens_latency,
         }
