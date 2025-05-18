c9b8a201c6f367e8844f7c1c4c3c626cce8e35ff
pavli <pavli@NOTEBOOK-PAVEL>
2025-05-18 14:08:32 +0200

    added table maker

--- AlignAtt/plot_results.py
+++ AlignAtt/plot_results.py
@@ -3,6 +3,13 @@ import os
 with open("best.json", "r") as f:
     data = json.load(f)
 
+def tostr(x):
+    if isinstance(x, float):
+        return f"{x:.2f}"
+    else:
+        return str(x)
+
+
 def make_table(headers, data):
     textabular = f"l|{'r'*len(headers)}"
     texheader = " & " + " & ".join(headers) + "\\\\"
@@ -10,7 +17,7 @@ def make_table(headers, data):
     for label in sorted(data, key=lambda x: data[x][0], reverse=True):
        if label == "z":
           texdata += "\\hline\n"
-       texdata += f"{label} & {' & '.join(map(str,data[label][:len(headers)]))} \\\\\n"
+       texdata += f"{label} & {' & '.join(map(tostr,data[label][:len(headers)]))} \\\\\n"
 
     print("\\begin{tabular}{"+textabular+"}")
     print(texheader)
@@ -18,7 +25,7 @@ def make_table(headers, data):
     print("\\end{tabular}")
 
 data = {os.path.splitext(os.path.basename(x[1]))[0].replace("_", " "): x[0] for x in data}
-data = {k: (v["all_metrics"]["bleu"], v["all_metrics"]["AL"], v["all_metrics"]["wer"], v["num_beams"], v["wait_for_beginning"], v["example_sentances"][0][len(v["example_sentances"][0])//2], v["example_sentances"][-1]) for k,v in data.items()}
+data = {k: (v["all_metrics"]["bleu"], v["all_metrics"]["wer"], v["all_metrics"]["AL"], v["num_beams"], v["wait_for_beginning"], v["example_sentances"][0][len(v["example_sentances"][0])//2], v["example_sentances"][-1]) for k,v in data.items()}
 
 make_table(["bleu", "word error rate", "average lagging", "number of beams","wait_for_beginning", "halfway example", "full example"], data)
 
