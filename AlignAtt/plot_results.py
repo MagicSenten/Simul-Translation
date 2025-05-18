57edf4cc0d58370a74b8239184a66a0a2881f969
pavli <pavli@NOTEBOOK-PAVEL>
2025-05-18 14:02:06 +0200

    added table maker

--- AlignAtt/plot_results.py
+++ AlignAtt/plot_results.py
@@ -17,8 +17,8 @@ def make_table(headers, data):
     print(texdata,end="")
     print("\\end{tabular}")
 
-data = {os.path.splitext(os.path.basename(x[1]))[0]: x[0] for x in data}
+data = {os.path.splitext(os.path.basename(x[1]))[0].replace("_", " "): x[0] for x in data}
 data = {k: (v["all_metrics"]["bleu"], v["all_metrics"]["AL"], v["all_metrics"]["wer"], v["num_beams"], v["wait_for_beginning"], v["example_sentances"][0][len(v["example_sentances"][0])//2], v["example_sentances"][-1]) for k,v in data.items()}
-make_table(["bleu", "word error rate", "average lagging", "num_beams","wait_for_beginning", "halfway example", "full example"], data)
+make_table(["bleu", "word error rate", "average lagging", "number of beams","wait_for_beginning", "halfway example", "full example"], data)
 
-make_table(["bleu", "word error rate", "average lagging", "num_beams","wait_for_beginning"], data)
+make_table(["bleu", "word error rate", "average lagging", "number of beams","wait k for beginning"], data)
