81ac13a2fb269c4e7df8b41ce58309aa837938a5
pavli <pavli@NOTEBOOK-PAVEL>
2025-05-18 14:04:23 +0200

    added table maker

--- AlignAtt/plot_results.py
+++ AlignAtt/plot_results.py
@@ -7,7 +7,7 @@ def make_table(headers, data):
     textabular = f"l|{'r'*len(headers)}"
     texheader = " & " + " & ".join(headers) + "\\\\"
     texdata = "\\hline\n"
-    for label in sorted(data):
+    for label in sorted(data, key=lambda x: data[x][0], reverse=True):
        if label == "z":
           texdata += "\\hline\n"
        texdata += f"{label} & {' & '.join(map(str,data[label][:len(headers)]))} \\\\\n"
@@ -19,6 +19,7 @@ def make_table(headers, data):
 
 data = {os.path.splitext(os.path.basename(x[1]))[0].replace("_", " "): x[0] for x in data}
 data = {k: (v["all_metrics"]["bleu"], v["all_metrics"]["AL"], v["all_metrics"]["wer"], v["num_beams"], v["wait_for_beginning"], v["example_sentances"][0][len(v["example_sentances"][0])//2], v["example_sentances"][-1]) for k,v in data.items()}
+
 make_table(["bleu", "word error rate", "average lagging", "number of beams","wait_for_beginning", "halfway example", "full example"], data)
 
 make_table(["bleu", "word error rate", "average lagging", "number of beams","wait k for beginning"], data)
