1a67bbc238aa974b089690d35f77de97d95021ae
pavli <pavli@NOTEBOOK-PAVEL>
2025-05-18 14:00:28 +0200

    added table maker

---
+++ AlignAtt/plot_results.py
@@ -0,0 +1,24 @@
+import json
+import os
+with open("best.json", "r") as f:
+    data = json.load(f)
+
+def make_table(headers, data):
+    textabular = f"l|{'r'*len(headers)}"
+    texheader = " & " + " & ".join(headers) + "\\\\"
+    texdata = "\\hline\n"
+    for label in sorted(data):
+       if label == "z":
+          texdata += "\\hline\n"
+       texdata += f"{label} & {' & '.join(map(str,data[label][:len(headers)]))} \\\\\n"
+
+    print("\\begin{tabular}{"+textabular+"}")
+    print(texheader)
+    print(texdata,end="")
+    print("\\end{tabular}")
+
+data = {os.path.splitext(os.path.basename(x[1]))[0]: x[0] for x in data}
+data = {k: (v["all_metrics"]["bleu"], v["all_metrics"]["AL"], v["all_metrics"]["wer"], v["num_beams"], v["wait_for_beginning"], v["example_sentances"][0][len(v["example_sentances"][0])//2], v["example_sentances"][-1]) for k,v in data.items()}
+make_table(["bleu", "word error rate", "average lagging", "num_beams","wait_for_beginning", "halfway example", "full example"], data)
+
+make_table(["bleu", "word error rate", "average lagging", "num_beams","wait_for_beginning"], data)
